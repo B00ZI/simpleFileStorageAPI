1107b37dab1b25d0c370afe674d6aedc9803db05
B00ZI <iraamaneyahya@gmail.com>
2026-01-10 22:20:19 +0100

    Refactor S3 file upload script and remove commented-out credentials

--- s3.php
+++ s3.php
@@ -1,12 +1,56 @@
-<!-- 
-applicationKey:
-K005OXOL5XUMeFshX4IBpcN/n5VfFzI
+<?php
 
-keyName:
-fileStorage-app
 
-keyID:
-0057fbd734b45860000000001 
 
-Endpoint:s3.us-east-005.backblazeb2.com
--->
+// Require the Composer autoloader.
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+
+$file = "t.txt";
+$content = "testing file content ";
+
+$applicationKey = "K005OXOL5XUMeFshX4IBpcN/n5VfFzI";
+$keyName =  "fileStorage-app";
+$keyID = "keyID";
+$Emdpoint = "s3.us-east-005.backblazeb2.com";
+
+$Bucket = "fileStorageLearning";
+
+
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-005',
+    'credentials' => [
+         'key' => $keyID,
+         'secret' => $applicationKey,
+    ],
+]);
+
+try {
+    $s3->putObject([
+        'Bucket' => $Bucket,
+        'Key'    => $file,
+        'Body'   => $content,
+       
+    ]);
+} catch (Aws\S3\Exception\S3Exception $e) {
+
+     echo "Error: " . $e->getAwsErrorMessage();
+}
+
+
+
+
+
+// applicationKey:
+// K005OXOL5XUMeFshX4IBpcN/n5VfFzI
+
+// keyName:
+// fileStorage-app
+
+// keyID:
+// 0057fbd734b45860000000001 
+
+// Endpoint:s3.us-east-005.backblazeb2.com
